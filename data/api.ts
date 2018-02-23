48fc43b4bd6e83b67276389aa887a2ae0e18c173
Tomasz Ducin <tomasz.ducin@gmail.com>
2018-02-23 23:44:36 +0100

    additional data

--- data/api.ts
+++ data/api.ts
@@ -2,30 +2,30 @@
 
 const API = (function () {
 
-  const timeout = () => Math.random() * 100
+	const timeout = () => Math.random() * 100
 
-  const wrapWithPromise = <T>(fn: Function) =>
-    (...args): Promise<T> => {
-      return new Promise((resolve, reject) =>
-        setTimeout(() => resolve(fn(...args)), timeout())
-      )
-    }
+	const wrapWithPromise = <T>(fn: Function) =>
+		(...args): Promise<T> => {
+			return new Promise((resolve, reject) =>
+				setTimeout(() => resolve(fn(...args)), timeout())
+			)
+		}
 
-  const getEmployee: (id: number) => Promise<Employee>
-    = wrapWithPromise(db.getEmployeeById);
+	const getEmployee: (id: number) => Promise<Employee>
+		= wrapWithPromise(db.getEmployeeById);
 
-  const getEmployeesByNationality: (nat: Nationality) => Promise<Employee[]>
-    = wrapWithPromise(db.getEmployeesByNationality);
+	const getEmployeesByNationality: (nat: Nationality) => Promise<Employee[]>
+		= wrapWithPromise(db.getEmployeesByNationality);
 
-  const getNationalities: () => Promise<Nationality[]>
-    = wrapWithPromise(db.getNationalities)
+	const getNationalities: () => Promise<Nationality[]>
+		= wrapWithPromise(db.getNationalities)
 
-  return {
-    // getEmployee(id): thenable
-    getEmployee,
-    // getEmployeesByNationality(nationality): thenable
-    getEmployeesByNationality,
-    // getNationalities(): thenable
-    getNationalities,
-  }
+	return {
+		// getEmployee(id): thenable
+		getEmployee,
+		// getEmployeesByNationality(nationality): thenable
+		getEmployeesByNationality,
+		// getNationalities(): thenable
+		getNationalities,
+	}
 }());
---
+++ data/data-shopping.ts
@@ -0,0 +1,12 @@
+const shoppingData = [
+	{ type: 'Clothes', name: 'Socks', price: 1.00, qty: 5, id: 421801449988 },
+	{ type: 'Clothes', name: 'Trousers', price: 3.90, qty: 2, id: 346859289079 },
+	{ type: 'Clothes', name: 'Pajams', price: 4.80, qty: 1, id: 522050190541 },
+	{ type: 'Clothes', name: 'Shoes', price: 23.00, qty: 2, id: 983521769712 },
+	{ type: 'Music', name: 'David Bowie', price: 11.90, qty: 1, id: 833864689518 },
+	{ type: 'Music', name: 'Rolling Stones', price: 8.95, qty: 1, id: 446941704202 },
+	{ type: 'Music', name: 'ABBA', price: 9.90, qty: 1, id: 609551649329 },
+	{ type: 'Food', name: 'Chips', price: 3.50, qty: 4, id: 206058323583 },
+	{ type: 'Food', name: 'Fish', price: 8.75, qty: 2, id: 837318618873 },
+	{ type: 'Food', name: 'Beer', price: 2.80, qty: 12, id: 611830716982 },
+];
